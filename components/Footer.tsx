31eba3a4816e53aa058d589194d8f34c27437187
stxndli <91648898+stxndli@users.noreply.github.com>
2022-11-14 09:15:34 +0100

    update github link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Footer(){
     return(
         <div className="flex flex-col mt-10 space-y-1 items-center justify-center w-full h-24 bg-[#242424]">
-            <Link href="https://github.com/stxndli/next-ecom">
+            <Link href="https://github.com/stxndli/ecom">
             <Image
                 src="/github.png"
                 alt="Github"
